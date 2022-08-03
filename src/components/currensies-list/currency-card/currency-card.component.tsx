282f24b891f7ab213b3d809baaaa8ef627457c85
makstarr <maksi112@icloud.com>
2022-08-03 12:34:15 +0300

    Unused files removed

--- src/components/currensies-list/currency-card/currency-card.component.tsx
+++ src/components/currensies-list/currency-card/currency-card.component.tsx
@@ -12,8 +12,8 @@ export const CurrencyCardComponent = ({ data: { ticker, flag, country, exchangeR
 	return (
 		<Styled.Card>
 			<Styled.Flag src={flags[flag]} alt={ticker} />
-			<Styled.Ticker>	{ticker}</Styled.Ticker>
-			<Styled.Country> {country}</Styled.Country>
+			<Styled.Ticker>{ticker}</Styled.Ticker>
+			<Styled.Country>{country}</Styled.Country>
 			<Styled.ExchangeRate>
 				{exchangeRate.toFixed(2)} <Styled.BaseCurrency>{ticker} = 1 <span>{baseCurrency}</span></Styled.BaseCurrency> 
 			</Styled.ExchangeRate>
